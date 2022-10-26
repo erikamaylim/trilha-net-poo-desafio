f44fe0fdf63fe9a2dd10afdbfe8dc6f6923a0602
erikamaylim <erikamayerlima@gmail.com>
2022-10-26 11:45:24 -0300

    Resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine($"Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo G21", "111111111", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("e-Título");
+
+Console.WriteLine("");
+
+Console.WriteLine($"Smartphone iPhone:");
+Smartphone iphone = new Iphone("987654", "Modelo 14 Pro", "222222222", 256);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Conecte SUS");
+
+
